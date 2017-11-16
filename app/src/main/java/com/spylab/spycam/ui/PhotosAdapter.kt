d476f8b073b2b4b1569a2040a948a1ce85f4d4b1
a.hatrus <>
2017-11-16 11:28:01 +0200

    clickerrrzzzzz

--- app/src/main/java/com/spylab/spycam/ui/PhotosAdapter.kt
+++ app/src/main/java/com/spylab/spycam/ui/PhotosAdapter.kt
@@ -17,9 +17,12 @@ import java.io.File
  * @author a.hatrus.
  */
 
-class PhotosAdapter(private var context: Context, private val imageWidth: Int) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class PhotosAdapter(private var context: Context, private val imageWidth: Int) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var photos: MutableList<File> = mutableListOf()
+    private var selectedItems: MutableList<Int> = mutableListOf()
+
     override fun getItemCount(): Int {
         return photos.size
     }
@@ -28,8 +31,14 @@ class PhotosAdapter(private var context: Context, private val imageWidth: Int) :
         var photo = photos[position]
         Glide.with(context)
                 .load(photo)
-                .apply(RequestOptions().centerCrop())
+                .apply(RequestOptions().centerCrop().sizeMultiplier(0.1f))
                 .into(holder?.itemView?.image)
+//
+//        if(selectedItems.contains(position)) {
+//            holder?.itemView?.imageSelected?.visibility = View.VISIBLE
+//        } else {
+//            holder?.itemView?.imageSelected?.visibility = View.GONE
+//        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
@@ -52,7 +61,23 @@ class PhotosAdapter(private var context: Context, private val imageWidth: Int) :
         notifyDataSetChanged()
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view),
+            View.OnClickListener, View.OnLongClickListener {
         private lateinit var image: ImageView
+        private lateinit var imageSelected: ImageView
+        override fun onClick(v: View?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onLongClick(v: View?): Boolean {
+//            val index = adapterPosition
+//            if (selectedItems.contains(index)) {
+//                selectedItems.remove(index)
+//            } else {
+//                selectedItems.add(index)
+//            }
+//            notifyDataSetChanged()
+            return false
+        }
     }
 }
